3f59539cba6edaafb7410bafed1a2092998fa5d2
Michael S. Klishin <michael@novemberain.com>
2012-09-19 17:11:57 +0400

    Port Blabbr
    from rubyamqp.info

---
+++ src/clojure/clojurewerkz/langohr/examples/blabbr.clj
@@ -0,0 +1,39 @@
+(ns clojurewerkz.langohr.examples.blabbr
+  (:gen-class)
+  (:require [langohr.core      :as rmq]
+            [langohr.channel   :as lch]
+            [langohr.queue     :as lq]
+            [langohr.exchange  :as le]
+            [langohr.consumers :as lc]
+            [langohr.basic     :as lb]))
+
+(def ^{:const true}
+  default-exchange-name "")
+
+
+
+(defn start-consumer
+  "Starts a consumer bound to the given topic exchange in a separate thread"
+  [ch topic-name username]
+  (let [queue-name (format "nba.newsfeeds.%s" username)
+        handler    (fn [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
+                     (println (format "[consumer] %s received %s" username (String. payload "UTF-8"))))]
+    (lq/declare ch queue-name :exclusive false :auto-delete true)
+    (lq/bind    ch queue-name topic-name)
+    (.start (Thread. (fn []
+                       (lc/subscribe ch queue-name handler :auto-ack true))))))
+
+(defn -main
+  [& args]
+  (let [conn  (rmq/connect)
+        ch    (lch/open conn)
+        ex    "nba.scores"
+        users ["joe" "aaron" "bob"]]
+    (le/declare ch ex "fanout" :durable false :auto-delete true)
+    (doseq [u users]
+      (start-consumer ch "nba.scores" u))
+    (lb/publish ch ex "" "BOS 101, NYK 89" :content-type "text/plain" :type "scores.update")
+    (lb/publish ch ex "" "ORL 85, ALT 88"  :content-type "text/plain" :type "scores.update")
+    (Thread/sleep 2000)
+    (rmq/close ch)
+    (rmq/close conn)))
