7ae8faf532283fb91f0f4614bd84f51eb02eaf63
Michael S. Klishin <michael@novemberain.com>
2012-09-24 05:51:31 +0400

    Use several server-named queues

--- src/clojure/clojurewerkz/langohr/examples/fanout_routing.clj
+++ src/clojure/clojurewerkz/langohr/examples/fanout_routing.clj
@@ -7,29 +7,29 @@
             [langohr.consumers :as lc]
             [langohr.basic     :as lb]))
 
-(defn message-handler
-  [ch metadata ^bytes payload]
-  (println (format "[consumer] Received a message: %s"
-                   (String. payload "UTF-8"))))
 
 (defn start-consumer
   "Starts a consumer in a separate thread"
   [ch queue-name]
-  (let [thread (Thread. (fn []
-                          (lc/subscribe ch queue-name message-handler :auto-ack true)))]
+  (let [handler (fn [ch metadata ^bytes payload]
+                  (println (format "[consumer] %s received a message: %s"
+                                   queue-name
+                                   (String. payload "UTF-8"))))
+        thread  (Thread. (fn []
+                           (lc/subscribe ch queue-name handler :auto-ack true)))]
     (.start thread)))
 
 (defn -main
   [& args]
   (let [conn  (rmq/connect)
         ch    (lch/open conn)
-        qname "langohr.examples.fanout-routing"
         ename "langohr.examples.fanout"]
-    (lq/declare ch qname :exclusive false :auto-delete true)
-    (start-consumer ch qname)
     (le/declare ch ename "fanout")
-    (lq/bind    ch qname ename)
-    (lb/publish ch ename qname "Ping" :content-type "text/plain")
+    (dotimes [i 10]
+      (let [q (.getQueue (lq/declare ch "" :exclusive false :auto-delete true))]
+        (lq/bind    ch q ename)
+        (start-consumer ch q)))
+    (lb/publish ch ename "" "Ping" :content-type "text/plain")
     (Thread/sleep 2000)
     (println "[main] Disconnecting...")
     (rmq/close ch)
