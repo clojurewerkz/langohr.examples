8935191532cd70a727a76e867ddd870ddbc40f9a
Michael S. Klishin <michael@novemberain.com>
2012-10-01 16:36:33 +0300

    Headers exchange routing example

--- project.clj
+++ project.clj
@@ -12,12 +12,13 @@
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :profiles {:hello-world    {:main clojurewerkz.langohr.examples.hello-world}
-             :blabbr         {:main clojurewerkz.langohr.examples.blabbr}
-             :weathr         {:main clojurewerkz.langohr.examples.weathr}
-             :redelivery     {:main clojurewerkz.langohr.examples.redelivery}
-             :fanout-routing {:main clojurewerkz.langohr.examples.fanout-routing}
-             :direct-routing {:main clojurewerkz.langohr.examples.direct-routing}
+  :profiles {:hello-world          {:main clojurewerkz.langohr.examples.hello-world}
+             :blabbr               {:main clojurewerkz.langohr.examples.blabbr}
+             :weathr               {:main clojurewerkz.langohr.examples.weathr}
+             :redelivery           {:main clojurewerkz.langohr.examples.redelivery}
+             :fanout-routing       {:main clojurewerkz.langohr.examples.fanout-routing}
+             :direct-routing       {:main clojurewerkz.langohr.examples.direct-routing}
+             :headers-routing      {:main clojurewerkz.langohr.examples.headers-routing}
              :mandatory-publishing {:main clojurewerkz.langohr.examples.mandatory-publishing}}
   :aliases  {"hello-world" ["with-profile" "hello-world"]
              "blabbr"      ["with-profile" "blabbr"]
@@ -25,5 +26,6 @@
              "redelivery"  ["with-profile" "redelivery"]
              "fanout-routing"  ["with-profile" "fanout-routing"]
              "direct-routing"  ["with-profile" "direct-routing"]
+             "headers-routing" ["with-profile" "headers-routing"]
              "mandatory-publishing"  ["with-profile" "mandatory-publishing"]}
   :jvm-opts ["-Xmx512m"])
---
+++ src/clojure/clojurewerkz/langohr/examples/headers_routing.clj
@@ -0,0 +1,40 @@
+(ns clojurewerkz.langohr.examples.headers-routing
+  (:gen-class)
+  (:require [langohr.core      :as rmq]
+            [langohr.channel   :as lch]
+            [langohr.exchange  :as le]
+            [langohr.queue     :as lq]
+            [langohr.consumers :as lc]
+            [langohr.basic     :as lb]))
+
+
+(defn start-consumer
+  "Starts a consumer in a separate thread"
+  [ch queue-name]
+  (let [handler (fn [ch metadata ^bytes payload]
+                  (println (format "[consumer] %s received a message: %s"
+                                   queue-name
+                                   (String. payload "UTF-8"))))
+        thread  (Thread. (fn []
+                           (lc/subscribe ch queue-name handler :auto-ack true)))]
+    (.start thread)))
+
+(defn -main
+  [& args]
+  (let [conn  (rmq/connect)
+        ch    (lch/open conn)
+        ename "langohr.examples.headers"]
+    (le/declare ch ename "headers")
+    (let [qname (.getQueue (lq/declare ch "" :auto-delete true :exclusive false))]
+      (lq/bind ch qname ename :arguments {"os" "linux" "cores" 8 "x-match" "all"})
+      (start-consumer ch qname))
+    (let [qname (.getQueue (lq/declare ch "" :auto-delete true :exclusive false))]
+      (lq/bind ch qname ename :arguments {"os" "osx" "cores" 4 "x-match" "any"})
+      (start-consumer ch qname))    
+    (lb/publish ch ename "" "For 8 cores Linux" :content-type "text/plain" :headers {"os" "linux" "cores" 8})
+    (lb/publish ch ename "" "For 8 cores Linux" :content-type "text/plain" :headers {"os" "osx"   "cores" 8})
+    (lb/publish ch ename "" "For 4 cores Linux" :content-type "text/plain" :headers {"os" "linux" "cores" 4})
+    (Thread/sleep 2000)
+    (println "[main] Disconnecting...")
+    (rmq/close ch)
+    (rmq/close conn)))
