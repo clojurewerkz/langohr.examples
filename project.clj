6d9419cfb938f309f4dcba4189777d876704568d
Michael Klishin <michael@defprotocol.org>
2013-07-31 00:47:24 +0400

    Upgrade to Langohr 1.1 and Clojure 1.5

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description "Various examples for Langohr documentation"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"}
-  :dependencies [[org.clojure/clojure     "1.4.0"]
-                 [com.novemberain/langohr "1.0.0-beta9"]]
+  :dependencies [[org.clojure/clojure     "1.5.1"]
+                 [com.novemberain/langohr "1.1.0"]]
   :source-paths      ["src/clojure"]
   :url "https://github.com/clojurewerkz/langohr.examples"
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
