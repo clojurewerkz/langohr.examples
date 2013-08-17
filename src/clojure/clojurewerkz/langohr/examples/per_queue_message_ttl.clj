218d9baa763815d1047640d3273f97c26be33807
Michael Klishin <michael@defprotocol.org>
2013-08-17 21:58:41 +0400

    Per queue message TTL example

---
+++ src/clojure/clojurewerkz/langohr/examples/per_queue_message_ttl.clj
@@ -0,0 +1,26 @@
+(ns clojurewerkz.langohr.examples.per-queue-message-ttl
+  (:gen-class)
+  (:require [langohr.core    :as rmq]
+            [langohr.channel :as lch]
+            [langohr.queue   :as lq]
+            [langohr.basic   :as lb]))
+
+(def ^{:const true}
+  default-exchange-name "")
+
+(defn -main
+  [& args]
+  (let [conn  (rmq/connect)
+        ch    (lch/open conn)
+        qname "clojurewerkz.langohr.examples.per-queue-message-ttl"]
+    (lq/declare ch qname :arguments {"x-message-ttl" 500} :durable false)
+    (lb/publish ch default-exchange-name qname "a message")
+    (Thread/sleep 50)
+    (println (format "Queue %s has %d messages" qname (lq/message-count ch qname)))
+    (println "Waiting for 600 ms")
+    (Thread/sleep 600)
+    (println (format "Queue %s has %d messages" qname (lq/message-count ch qname)))
+    (println "[main] Disconnecting...")
+    (when (rmq/open? ch)
+      (rmq/close ch))
+    (rmq/close conn)))
