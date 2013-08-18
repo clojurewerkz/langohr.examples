23cad334fa27b80bec083f67455e51f021621bf3
Michael Klishin <michael@defprotocol.org>
2013-08-18 20:29:32 +0400

    Consumer cancellation notification example

---
+++ src/clojure/clojurewerkz/langohr/examples/consumer_cancel_notifications.clj
@@ -0,0 +1,28 @@
+(ns clojurewerkz.langohr.examples.consumer-cancel-notifications
+  (:gen-class)
+  (:require [langohr.core      :as rmq]
+            [langohr.channel   :as lch]
+            [langohr.queue     :as lq]
+            [langohr.exchange  :as lx]
+            [langohr.basic     :as lb]
+            [langohr.consumers :as lcons])
+  (:import [java.util.concurrent CountDownLatch TimeUnit]))
+
+(defn -main
+  [& args]
+  (let [conn  (rmq/connect)
+        ch    (lch/open conn)
+        q     (lq/declare-server-named ch)
+        latch (CountDownLatch. 1)]
+    (lcons/subscribe ch q
+                     (fn [ch {:keys [delivery-tag]} ^bytes payload]
+                       (comment "No op"))
+                     :auto-ack true
+                     :handle-cancel-fn (fn [consumer-tag]
+                                         (println (format "Consumer %s has been cancelled" consumer-tag))
+                                         (.countDown latch)))
+    (lq/delete ch q)
+    (.await latch 200 TimeUnit/MILLISECONDS)
+    (println "[main] Disconnecting...")
+    (rmq/close ch)
+    (rmq/close conn)))
