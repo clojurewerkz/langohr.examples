85874b27b81ca2958df546019f5a70bfa267423c
Michael Klishin <michael@defprotocol.org>
2013-08-18 20:57:03 +0400

    Publisher confirms example

---
+++ src/clojure/clojurewerkz/langohr/examples/publisher_confirms.clj
@@ -0,0 +1,25 @@
+(ns clojurewerkz.langohr.examples.publisher-confirms
+  (:gen-class)
+  (:require [langohr.core      :as rmq]
+            [langohr.channel   :as lch]
+            [langohr.confirm   :as lcf]
+            [langohr.queue     :as lq]
+            [langohr.exchange  :as lx]
+            [langohr.basic     :as lb]))
+
+(def ^{:const true}
+  default-exchange-name "")
+
+(defn -main
+  [& args]
+  (let [conn  (rmq/connect)
+        ch    (doto (lch/open conn)
+                (lcf/select))
+        q     (lq/declare-server-named ch)]
+    (dotimes [n 1000]
+      (lb/publish ch default-exchange-name q "msg"))
+    (.waitForConfirms ch)
+    (println "All confirms arrived...")
+    (println "[main] Disconnecting...")
+    (rmq/close ch)
+    (rmq/close conn)))
