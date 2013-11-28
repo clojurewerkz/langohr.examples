050a79b53098240c61954c20760732fe64bb9793
Michael Klishin <michael@clojurewerkz.org>
2013-11-28 15:18:45 +0400

    Bump Langohr to 1.6

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure     "1.5.1"]
-                 [com.novemberain/langohr "1.4.1"]]
+                 [com.novemberain/langohr "1.6.0"]]
   :source-paths  ["src/clojure"]
   :url "https://github.com/clojurewerkz/langohr.examples"
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
