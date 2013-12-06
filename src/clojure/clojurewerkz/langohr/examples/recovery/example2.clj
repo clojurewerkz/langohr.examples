d45a28036eceff70f27a41354f94f1a40b0e3125
Michael Klishin <michael@clojurewerkz.org>
2013-12-06 05:37:41 +0400

    Ditto

--- src/clojure/clojurewerkz/langohr/examples/recovery/example2.clj
+++ src/clojure/clojurewerkz/langohr/examples/recovery/example2.clj
@@ -51,7 +51,7 @@
                   :auto-ack true)
 
     (while true
-      (Thread/sleep 2000)
+      (Thread/sleep 500)
       (try
         (let [rk (rand-nth ["abc" "def" "xyz" "123"])]
           (println (format "Publishing a message with routing key %s" rk))
