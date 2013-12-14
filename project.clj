d4f254c5efcac9177d7978c04e1cfd0a3e8eaea6
Michael Klishin <michael@clojurewerkz.org>
2013-12-14 17:31:22 +0400

    Depend on Langohr 2.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure     "1.5.1"]
-                 [com.novemberain/langohr "1.7.0"]]
+                 [com.novemberain/langohr "2.0.0-rc4"]]
   :source-paths  ["src/clojure"]
   :url "https://github.com/clojurewerkz/langohr.examples"
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
