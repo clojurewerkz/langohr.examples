a2c8bc341d006cac7ab41e65822321d73f09bdcd
Michael Klishin <michael@clojurewerkz.org>
2014-08-22 16:37:49 +0400

    Adapt DLX example

--- src/clojure/clojurewerkz/langohr/examples/dead_letter_exchange.clj
+++ src/clojure/clojurewerkz/langohr/examples/dead_letter_exchange.clj
@@ -16,10 +16,10 @@
         q1    "clojurewerkz.langohr.examples.dlx.q1"
         q2    "clojurewerkz.langohr.examples.dlx.q2"
         dlx   "clojurewerkz.langohr.examples.dlx"]
-    (lq/declare ch q1 :durable false :arguments {"x-dead-letter-exchange" dlx
-                                                 "x-message-ttl" 300})
-    (lq/declare ch q2 :durable false)
-    (lx/fanout ch dlx :durable false)
+    (lq/declare ch q1 {:durable false :arguments {"x-dead-letter-exchange" dlx
+                                                  "x-message-ttl" 300}})
+    (lq/declare ch q2 {:durable false})
+    (lx/fanout ch dlx {:durable false})
     (lq/bind ch q2 dlx)
     (lb/publish ch default-exchange-name q1 "a message")
     ;; expired messages are dead lettered
