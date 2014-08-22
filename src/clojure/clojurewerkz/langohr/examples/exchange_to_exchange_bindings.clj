c58c4c74bd678e8b73e5a2deb75a68ecbd96d51e
Michael Klishin <michael@clojurewerkz.org>
2014-08-22 17:01:07 +0400

    Adapt E2E binding example

--- src/clojure/clojurewerkz/langohr/examples/exchange_to_exchange_bindings.clj
+++ src/clojure/clojurewerkz/langohr/examples/exchange_to_exchange_bindings.clj
@@ -13,11 +13,11 @@
         x1    "clojurewerkz.langohr.examples.dlx.x1"
         x2    "clojurewerkz.langohr.examples.dlx.x2"
         qname "clojurewerkz.langohr.examples.dlx.q"]
-    (lx/direct ch x1 :durable false)
-    (lx/fanout ch x2 :durable false)
-    (lq/declare ch qname :exclusive true)
+    (lx/direct ch x1 {:durable false})
+    (lx/fanout ch x2 {:durable false})
+    (lq/declare ch qname {:exclusive true})
     (lq/bind ch qname x2)
-    (lx/bind ch x2 x1 :routing-key "unsorted")
+    (lx/bind ch x2 x1 {:routing-key "unsorted"})
     (lb/publish ch x1 "unsorted" "a message")
     (Thread/sleep 50)
     (println (format "Queue %s has %d message(s)" qname (lq/message-count ch qname)))
