d9a6bf9a872f08f630f1f107d928c3cc61fbfdb4
Michael Klishin <michael@clojurewerkz.org>
2014-08-23 12:51:36 +0400

    rc2 is out

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :min-lein-version "2.4.3"
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure     "1.6.0"]
-                 [com.novemberain/langohr "3.0.0-rc1"]]
+                 [com.novemberain/langohr "3.0.0-rc2"]]
   :source-paths  ["src/clojure"]
   :url "https://github.com/clojurewerkz/langohr.examples"
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
