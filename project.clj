353ebe35a80746108335b52669a67b8043b389e3
Michael Klishin <michael@clojurewerkz.org>
2015-01-10 18:24:38 +0300

    Bump Leiningen requirement to 2.5.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojurewerkz/langohr.examples "1.0.0-SNAPSHOT"
   :description "Various examples for Langohr documentation"
-  :min-lein-version "2.5.0"
+  :min-lein-version "2.5.1"
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure     "1.6.0"]
                  [com.novemberain/langohr "3.0.1"]]
